fc3888bcf65d058a4acd1ba78899fb13ca0c8f68
Mercy <maritimmercy8@gmail.com>
2025-06-11 11:31:29 +0300

    added CRUD

--- server/models.py
+++ server/models.py
@@ -14,8 +14,8 @@ class Pet(db.Model):
     __tablename__ = 'pets'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    species = db.Column(db.String)
+    name = db.Column(db.String,nullable=False)
+    species = db.Column(db.String,nullable=False)
 
     def __repr__(self):
         return f'<Pet {self.id}, {self.name}, {self.species}>'
